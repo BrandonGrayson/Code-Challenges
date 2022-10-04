b42b28dbbaa3e9947cea8a293b14cd6cd4c6be1a
Brandon <brandonbgrayson@gmail.com>
2022-10-04 11:14:00 -0400

    noSwaps check implemented in bubble sort

--- BubbleSort/index.js
+++ BubbleSort/index.js
@@ -1,14 +1,16 @@
 function bubbleSort(arr, ind1, ind2) {
+    let noSwaps = true
     for (let i = arr.length; i > 0; i--) {
         for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+                noSwaps = false
                 // [arr[j], arr[j+ 1]] = [arr[j + 1], arr[j]]
             }
         }
+        if (noSwaps) break;
     }
-
     return arr
 }
