ee4799c8290f3a6dc27d266c091c1fa7e7dd9e11
Brandon <brandonbgrayson@gmail.com>
2022-11-16 10:13:04 -0500

    insert method added to Singly Linked List class

--- SinglyLinkedList/index.js
+++ SinglyLinkedList/index.js
@@ -92,7 +92,34 @@ class SinglyLinkedList {
         }
 
         return false
+    }
 
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false
+
+        if (index === this.length) {
+            this.push(val)
+            return true
+        }
+
+        if (index === 0) {
+            this.unShift(val)
+            return true
+        }
+
+        let newNode = new Node(val)
+
+        let prev = this.get(index - 1)
+
+        let temp = prev.next
+
+        prev.next = newNode
+
+        newNode.next = temp
+
+        this.length++
+
+        return true
 
     }
 }
