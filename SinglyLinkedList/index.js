05664cc0b268dbe1f5b46655f43af691a10dbd66
Brandon <brandonbgrayson@gmail.com>
2022-11-23 08:54:18 -0500

    remove method added to singly linked list class

--- SinglyLinkedList/index.js
+++ SinglyLinkedList/index.js
@@ -122,4 +122,14 @@ class SinglyLinkedList {
         return true
 
     }
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+        let prevNode = this.get(index - 1)
+        let removed = prevNode.next
+        prevNode.next = removed.next
+        this.length--
+        return removed
+    }
 }
